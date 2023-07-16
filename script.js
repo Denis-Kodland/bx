ecac417a384fae5dce957a5bce01e73ef2803258
Denis-Kodland <109456414+Denis-Kodland@users.noreply.github.com>
2023-07-16 17:31:53 +0300

    Add files into project

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slider').bxSlider( {
+        slideWidth: 700,
+		pagerCustom: '.slider-nav',
+		infiniteLoop: false,
+		hideControlOnEnd: true,
+    }
+    );
+});
